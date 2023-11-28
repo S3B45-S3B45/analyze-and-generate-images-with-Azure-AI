ae22d20d36d928f99382d6538a602999fe328991
Sebastian <sebas34571@gmail.com>
2023-11-28 08:33:31 -0500

    Add computer vision functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [url, setUrl] = useState('');
+
+  return (
+    <div>
+      <h1>Computer Vision</h1>
+      <input 
+        type="text" 
+        value={url} 
+        onChange={(e) => setUrl(e.target.value)} 
+        placeholder="Type the URL address here"
+      />
+      <button>Analyze</button>
+      <button>Generate</button>
+    </div>
+  );
 }
 
 export default App;
